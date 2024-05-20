e3f5674797c0c2a07f24867cdd4a3f68676e1483
Erlend Lind Madsen <erlendf80@gmail.com>
2024-05-20 14:01:53 +0200

    export default

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-// index.js
-import { sendFile } from './uSendFile.js';
+import fs from 'node:fs/promises';
+import mime from 'mime-types';
+
+function sendFile(filePath, res) {
+	res.onAborted(() => {
+		res.aborted = true;
+	});
+	console.log(`send file -> ${filePath}`)
+	fs.readFile(filePath).then(data => {
+		if (!res.aborted) {
+			res.cork(() => {
+				res.writeStatus('200')
+				res.writeHeader('Content-Type', mime.lookup(filePath))
+				res.end(data)
+			})
+		}
+	}).catch(err => {
+		console.log(err);
+		if (!res.aborted) {
+			res.cork(() => {
+				res.writeStatus('404')
+				res.end()
+			})
+		}
+	})
+}
+
+export default sendFile
